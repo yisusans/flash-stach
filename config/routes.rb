44399b7616026dfd55c55815418ee95c77058e8c
Susan Yi <yi.susans@gmail.com>
2016-06-13 20:26:16 -0400

    Login/Logout Errors
    Getting errors for logging out.  Can't seem to figure it out.  But also, the login method doesn't seem to be working well either.  Don't really understand what is going on.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
   resources :users, :except => [:index]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
